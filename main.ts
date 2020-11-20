d1e4fa6361b5e50d79bb5d1c14ec33891eb138d8
ninjahru <74370809+ninjahru@users.noreply.github.com>
2020-11-19 16:03:43 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,61 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (sprite, location) {
+    if (info.score() > 0) {
+        game.over(true)
+    }
+    
+    
+})
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    otherSprite.startEffect(effects.spray)
+    info.setScore(1)
+    info.startCountdown(12)
+})
+
+tiles.setTilemap(tilemap`level`)
+
+let player = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . f f e 2 f f f f f f 2 e f f . 
+    . f f f f f e e e e f f f f f . 
+    . . f e f b f 4 4 f b f e f . . 
+    . f f e 4 1 f d d f 1 4 e f . . 
+    f d f f e 4 d d d d 4 e f e . . 
+    f b f e f 2 2 2 2 e d d 4 e . . 
+    f b f 4 f 2 2 2 2 e d d e . . . 
+    f c f . f 4 4 5 5 f e e . . . . 
+    . f f . f f f f f f f . . . . . 
+    . . . . f f f . . . . . . . . . 
+    `, SpriteKind.Player)
+
+controller.moveSprite(player)
+
+tiles.placeOnRandomTile(player, sprites.castle.tileGrass2)
+
+scene.cameraFollowSprite(player)
+
+let food = sprites.create(img`
+    . . . . . . . e e e e . . . . . 
+    . . . . . e e 4 5 5 5 e e . . . 
+    . . . . e 4 5 6 2 2 7 6 6 e . . 
+    . . . e 5 6 6 7 2 2 6 4 4 4 e . 
+    . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
+    . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
+    . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
+    e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
+    e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
+    e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
+    e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
+    e 5 e c 5 4 5 4 5 5 5 e e . . . 
+    e 5 e e 5 5 5 5 5 4 e . . . . . 
+    4 5 4 e 5 5 5 5 e e . . . . . . 
+    . 4 5 4 5 5 4 e . . . . . . . . 
+    . . 4 4 e e e . . . . . . . . . 
+    `, SpriteKind.Food)
+tiles.placeOnRandomTile(food, sprites.castle.tileGrass2)
---
+++ tilemap.g.ts
@@ -0,0 +1,31 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`1000100001010101010101010101010101010101010101010101010101010101010101010101030303030303030303030304010101010304040404040404040404040101010103040303030303040404040401010101030403040404040303030304010101010304030404040303040403030201010103040304040403040404040301010101030403040404040404040403010101010304030404030304040403030101010103040303030304040404030101010101030404040404040404040301010101010303030303030303030303010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 2 . . . . . . . . . . . . . 
+. . 2 . 2 2 2 2 2 . . . . . . . 
+. . 2 . 2 . . . . 2 2 2 2 . . . 
+. . 2 . 2 . . . 2 2 . . 2 2 . . 
+. . 2 . 2 . . . 2 . . . . 2 . . 
+. . 2 . 2 . . . 2 . . . . 2 . . 
+. . 2 . 2 . . 2 2 . . . 2 2 . . 
+. . 2 . 2 2 2 2 . . . . 2 . . . 
+. . 2 . . . . . . . . . 2 . . . 
+. . 2 2 2 2 2 2 2 2 2 2 2 . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.builtin.forestTiles0,sprites.dungeon.collectibleInsignia,sprites.castle.tileDarkGrass2], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
